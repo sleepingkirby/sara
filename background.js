b2b6e0b43bc353e0df03f255d7661e2e0f3533a8
sleepingkirby@gmail.com <sleepingkirby@gmail.com>
2021-05-09 11:19:11 +0800

    added some checks to make sure info- paste context menus doesn't error if the right click menu gets desynced for whatever reason

--- background.js
+++ background.js
@@ -454,10 +454,14 @@ chrome.webNavigation.onCompleted.addListener(function(details){
   //adds listeners for the right click/context menu so we know what to do if something is clicked
   chrome.contextMenus.onClicked.addListener(function(info, tabs) {
       // if info.menuItemId starts with "info-", the action is to copy the data into the clipboard
+      console.log(cntxtCch);
       if(info.menuItemId.substr(0,5) == "info-"){
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-          chrome.tabs.sendMessage(tabs[0].id, {action: "sendInfo", msg:{attr:cntxtCch[info.menuItemId].attr,val:cntxtCch[info.menuItemId].val}},(e)=>{chromeSendMsgErrHndlDtl("sendInfo", details);});
-        });
+      console.log(cntxtCch.hasOwnProperty(info.menuItemId));
+        if(cntxtCch.hasOwnProperty(info.menuItemId) && cntxtCch[info.menuItemId].hasOwnProperty("attr") && cntxtCch[info.menuItemId].hasOwnProperty("val")){
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "sendInfo", msg:{attr:cntxtCch[info.menuItemId].attr,val:cntxtCch[info.menuItemId].val}},(e)=>{chromeSendMsgErrHndlDtl("sendInfo", details);});
+          });
+        }
       }
       else if(info.menuItemId.substr(0,6) == "paste-"){
         chrome.tabs.sendMessage(tabs.id, {action: "pasteVal", msg:{path:info.menuItemId}},(e)=>{chromeSendMsgErrHndlDtl("pasteVal", details);});
